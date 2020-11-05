10ddc243fbf2e717887e7e42853fd825db7a4b4d
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-11-05 23:28:44 +0100

    simple sigup flow

--- src/signup.js
+++ src/signup.js
@@ -2,41 +2,51 @@
 
 class Signup {
     constructor() {
-      this.nameInput = document.querySelector("#name");
-      this.emailInput = document.querySelector("#email");
-      this.passwordInput = document.querySelector("#password");
-      this.repeatPasswordInput = document.querySelector("#repeat-password");
+      this.nameInput = document.querySelector("#name-signup");
+      this.emailInput = document.querySelector("#email-signup");
+      this.passwordInput = document.querySelector("#password-signup");
+      this.repeatPasswordInput = document.querySelector("#repeat-password-signup");
       this.buttonInput = document.querySelector("#signup-button");
-      this.errorsWrapper = document.querySelector(".message-container");
+      //this.errorsWrapper = document.querySelector(".message-container");
     }
 
-    handleEmailInput = (event) => {
-      const email = event.target.value;  
-      validator.validateValidEmail(email);  
+    validateEmail = (event) => {
+        console.log("email!!!")
+        const email = event.target.value;  
+        console.log(email)
+        validator.validateValidEmail(email);  
+        
       const errors = validator.getErrors(); 
+        console.log(errors)
        if (!errors.invalidEmailError) {        
         validator.validateUniqueEmail(email);
       }
-      this.setErrorMessages();
-      this.checkButton();
+      //this.setErrorMessages();
+      //this.checkButton();
     }
 
-    handlePasswordInput = (event) => {
+    validatePassword = (event) => {
+        console.log('passqoefsaf!')
       const password = event.target.value;
+      console.log(password)
       const passwordRepeat = this.repeatPasswordInput.value;
       validator.validatePassword(password);
       validator.validatePasswordRepeat(password, passwordRepeat);
-      this.setErrorMessages();
-      this.checkButton();
+      console.log(validator.getErrors())
+      //this.setErrorMessages();
+      //this.checkButton();
     }
 
     handleRepeatPasswordInput = (event) => {
+        console.log('repeat passqoefsaf!')
+
       const passwordRepeat = event.target.value;
+      console.log(passwordRepeat)
       const password = this.passwordInput.value;
       validator.validatePassword(password);
       validator.validatePasswordRepeat(password, passwordRepeat);
-      this.setErrorMessages();
-      this.checkButton();
+      //this.setErrorMessages();
+      //this.checkButton();
     }
 
     saveData = (event) => {
@@ -51,60 +61,66 @@ class Signup {
       this.emailInput.value = "";
       this.passwordInput.value = "";
       this.repeatPasswordInput.value = "";
-      this.showSuccessMessage();
-      this.removeMessages();
+    //   //this.showSuccessMessage();
+    //   //this.removeMessages();
       validator.resetValidator();
-      this.buttonInput.disabled = true;
-      window.location.href="/index.html"
+    //   //this.buttonInput.disabled = true;
+    //   //window.location.href="/index.html"
+      console.log('Eureeeeekaaa chin!!!')
     }
     
     addListeners = () => {
-      this.emailInput.addEventListener("input", this.handleEmailInput);
-      this.passwordInput.addEventListener("input", this.handlePasswordInput);
+      console.log('adding!!!')
+
+      this.emailInput.addEventListener("input", this.validateEmail);
+      this.passwordInput.addEventListener("input", this.validatePassword);
       this.repeatPasswordInput.addEventListener("input", this.handleRepeatPasswordInput);
       this.buttonInput.addEventListener("click", this.saveData);
+      console.log('uooooo!!!')
+
     }
     
-    showSuccessMessage = () => {
-      this.errorsWrapper.innerHTML = "";  
-      const errorsObj = validator.getErrors();      
-      const errorsStringsArr = Object.values(errorsObj);  
-      if (errorsStringsArr.length > 1) {
-        return;
-      }
-      const successMessageP = document.createElement('p');
-      successMessageP.innerHTML = "La cuenta ha sido creada con exito";  
-      this.errorsWrapper.appendChild(successMessageP);
-    }
+    // showSuccessMessage = () => {
+    //   this.errorsWrapper.innerHTML = "";  
+    //   const errorsObj = validator.getErrors();      
+    //   const errorsStringsArr = Object.values(errorsObj);  
+    //   if (errorsStringsArr.length > 1) {
+    //     return;
+    //   }
+    //   const successMessageP = document.createElement('p');
+    //   successMessageP.innerHTML = "La cuenta ha sido creada con exito";  
+    //   this.errorsWrapper.appendChild(successMessageP);
+    // }
     
-    checkButton = () => {
-      const errorsObj = validator.getErrors();
-      const errorsArr = Object.values(errorsObj);  
-      if(errorsArr.length > 0) {
-        this.buttonInput.disabled = true;
-      }
-      else {
-        this.buttonInput.disabled = false;
-      }
-    }
+    // checkButton = () => {
+    //   const errorsObj = validator.getErrors();
+    //   const errorsArr = Object.values(errorsObj);  
+    //   if(errorsArr.length > 0) {
+    //     this.buttonInput.disabled = true;
+    //   }
+    //   else {
+    //     this.buttonInput.disabled = false;
+    //   }
+    // }
     
-    removeMessages = () => {
-      setTimeout( () => {
-        this.errorsWrapper.innerHTML = "";
-      }, 2000)
-    }
+    // removeMessages = () => {
+    //   setTimeout( () => {
+    //     this.errorsWrapper.innerHTML = "";
+    //   }, 2000)
+    // }
     
-    setErrorMessages = () => {     
-      this.errorsWrapper.innerHTML = "";     
-      const errorsObj = validator.getErrors(); 
-      const errorsStringsArr = Object.values(errorsObj);
-      errorsStringsArr.forEach( (errorStr) => {
-        const errorMessageP = document.createElement('p');
-        errorMessageP.innerHTML = errorStr;
-        this.errorsWrapper.appendChild(errorMessageP);
-      })
-    }
+    // setErrorMessages = () => {     
+    //   this.errorsWrapper.innerHTML = "";     
+    //   const errorsObj = validator.getErrors(); 
+    //   const errorsStringsArr = Object.values(errorsObj);
+    //   errorsStringsArr.forEach( (errorStr) => {
+    //     const errorMessageP = document.createElement('p');
+    //     errorMessageP.innerHTML = errorStr;
+    //     this.errorsWrapper.appendChild(errorMessageP);
+    //   })
+    // }
   }
   
   let signup = new Signup();
+  console.log("caquita")
   window.addEventListener("load", signup.addListeners);
