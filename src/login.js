63547b7d662c0809a95b8c9ef87f0f17957bebb7
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-11-05 22:41:57 +0100

    simple login flow

---
+++ src/login.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class Login {
+  constructor() {
+    this.emailInput = document.querySelector("#email");    
+    this.passwordInput = document.querySelector("#password");
+    this.loginButton = document.querySelector("#login-button");
+    //this.messageContainer = document.querySelector(".message-container");
+  }
+  submit = (event) => {
+    event.preventDefault();
+    const usersDB = db.getAllUsers();
+    const email = this.emailInput.value;
+    console.log(email)
+    const password = this.passwordInput.value;
+    console.log(password)
+    const user = usersDB.find( (userObj) => {
+      if (userObj.email === email && userObj.password === password) {
+        return true;
+      }
+    })
+    console.log('Eureka!!!')
+    this.showMessage(user);
+  }
+
+  showMessage = (user) => {  
+    //this.messageContainer.innerHTML = "";
+    //const message = document.createElement('p');
+    if (user) {
+    //   message.innerHTML = `hola, ${user.email}`;
+    //   message.classList.add("correct-message");
+    }
+    else {
+      //message.innerHTML = 'el email o/y password son incorectos';
+    }
+    // this.messageContainer.appendChild(message);
+    
+    //if (user) this.redirect();
+  }
+
+  redirect = () => {
+    setTimeout( ()=> location.assign('index.html'), 500);
+  }
+}
+const login = new Login();
+login.loginButton.addEventListener("click", login.submit);
---
+++ src/signup.js
@@ -0,0 +1,110 @@
+"use strict";
+
+class Signup {
+    constructor() {
+      this.nameInput = document.querySelector("#name");
+      this.emailInput = document.querySelector("#email");
+      this.passwordInput = document.querySelector("#password");
+      this.repeatPasswordInput = document.querySelector("#repeat-password");
+      this.buttonInput = document.querySelector("#signup-button");
+      this.errorsWrapper = document.querySelector(".message-container");
+    }
+
+    handleEmailInput = (event) => {
+      const email = event.target.value;  
+      validator.validateValidEmail(email);  
+      const errors = validator.getErrors(); 
+       if (!errors.invalidEmailError) {        
+        validator.validateUniqueEmail(email);
+      }
+      this.setErrorMessages();
+      this.checkButton();
+    }
+
+    handlePasswordInput = (event) => {
+      const password = event.target.value;
+      const passwordRepeat = this.repeatPasswordInput.value;
+      validator.validatePassword(password);
+      validator.validatePasswordRepeat(password, passwordRepeat);
+      this.setErrorMessages();
+      this.checkButton();
+    }
+
+    handleRepeatPasswordInput = (event) => {
+      const passwordRepeat = event.target.value;
+      const password = this.passwordInput.value;
+      validator.validatePassword(password);
+      validator.validatePasswordRepeat(password, passwordRepeat);
+      this.setErrorMessages();
+      this.checkButton();
+    }
+
+    saveData = (event) => {
+      event.preventDefault();
+      const name = this.nameInput.value;
+      const email = this.emailInput.value;
+      const password = this.passwordInput.value;
+      const repeatPassword = this.repeatPasswordInput.value;
+      const newUser = new User(name, email, password);
+      db.saveNewUser(newUser);
+      this.nameInput.value = "";
+      this.emailInput.value = "";
+      this.passwordInput.value = "";
+      this.repeatPasswordInput.value = "";
+      this.showSuccessMessage();
+      this.removeMessages();
+      validator.resetValidator();
+      this.buttonInput.disabled = true;
+      window.location.href="/index.html"
+    }
+    
+    addListeners = () => {
+      this.emailInput.addEventListener("input", this.handleEmailInput);
+      this.passwordInput.addEventListener("input", this.handlePasswordInput);
+      this.repeatPasswordInput.addEventListener("input", this.handleRepeatPasswordInput);
+      this.buttonInput.addEventListener("click", this.saveData);
+    }
+    
+    showSuccessMessage = () => {
+      this.errorsWrapper.innerHTML = "";  
+      const errorsObj = validator.getErrors();      
+      const errorsStringsArr = Object.values(errorsObj);  
+      if (errorsStringsArr.length > 1) {
+        return;
+      }
+      const successMessageP = document.createElement('p');
+      successMessageP.innerHTML = "La cuenta ha sido creada con exito";  
+      this.errorsWrapper.appendChild(successMessageP);
+    }
+    
+    checkButton = () => {
+      const errorsObj = validator.getErrors();
+      const errorsArr = Object.values(errorsObj);  
+      if(errorsArr.length > 0) {
+        this.buttonInput.disabled = true;
+      }
+      else {
+        this.buttonInput.disabled = false;
+      }
+    }
+    
+    removeMessages = () => {
+      setTimeout( () => {
+        this.errorsWrapper.innerHTML = "";
+      }, 2000)
+    }
+    
+    setErrorMessages = () => {     
+      this.errorsWrapper.innerHTML = "";     
+      const errorsObj = validator.getErrors(); 
+      const errorsStringsArr = Object.values(errorsObj);
+      errorsStringsArr.forEach( (errorStr) => {
+        const errorMessageP = document.createElement('p');
+        errorMessageP.innerHTML = errorStr;
+        this.errorsWrapper.appendChild(errorMessageP);
+      })
+    }
+  }
+  
+  let signup = new Signup();
+  window.addEventListener("load", signup.addListeners);
